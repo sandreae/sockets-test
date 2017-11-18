31b980f58d1006d7c30b9e49cbb6b375b3b6b239
Sam Andreae <sandreae@gmail.com>
2017-11-18 14:44:02 +0100

    array of videos and audio

--- server.js
+++ server.js
@@ -9,13 +9,25 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+
+var track = 0;
+var myIndex = 0;
+var myArray = [0, 1]
+
+function nextElement() {
+  track = myArray[myIndex];
+  myIndex = (myIndex+1)%(myArray.length);
+ }
+
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('bang', function(){
-    console.log('bang');
-    io.emit('play');
+  	console.log('bang');
+      io.emit("stop", track);
+      nextElement()
+      io.emit("play", track);
   });
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
