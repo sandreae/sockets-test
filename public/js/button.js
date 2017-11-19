84c10f24f792158320a46a67adfebc0e2710ab6b
Sam Andreae <sandreae@gmail.com>
2017-11-19 14:38:32 +0100

    fix play video for mobile

--- public/js/button.js
+++ public/js/button.js
@@ -1,9 +1,10 @@
-  button.addEventListener('click', function(){
+  function buttonPress(){
 	  for(var i = 0; i < scenes.length; i++) {
 	    var scene = scenes.item(i)
+	    scene.style.display = 'inline'
 	    scene.play()
 	    scene.pause()
 	    console.log("Scene " + i + " tested")
 	  }
     button.style.display = "none"
-  });
+  }
