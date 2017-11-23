04212f2cafe1990070450eeec7e9869178a7d0f8
Sam Andreae <sandreae@gmail.com>
2017-11-23 13:19:25 +0100

    fridge-template check

--- public/js/scenes/fridge.js
+++ public/js/scenes/fridge.js
@@ -1,11 +1,11 @@
-var videoFiles = ['black', 'drillsmall', 'fire', 'man', 'snow', 'string', 'wrap']
-var audioFiles = ['fire', 'fridge', 'gogogo', 'organ', 'squeak']
-var imageFiles = ['fuck', 'green', 'template']
-
+var videoFiles = []
+var audioFiles = []
+var imageFiles = ['fridge-template']
 
 var scenes = [
-  {video: 'black-video', audio: 'squeak-audio'},
-  {video: 'string-video', audio: 'organ-audio'},
-  {video: 'black-video', audio: 'squeak-audio'},
-  {video: 'string-video', audio: 'organ-audio'}
+  {video: 'fridge-template-image', audio: ''},
+  {video: 'fridge-template-image', audio: ''},
+  {video: 'fridge-template-image', audio: ''},
+  {video: 'fridge-template-image', audio: ''},
+  {video: 'fridge-template-image', audio: ''}
 ]
