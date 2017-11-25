239f4d5bf76efc6bdc483e6face47a48e78a8f5b
Sam Andreae <sandreae@gmail.com>
2017-11-25 17:35:36 +0100

    new images

--- public/js/scenes/fridge.js
+++ public/js/scenes/fridge.js
@@ -1,6 +1,6 @@
 var videoFiles = ['fire', 'drillsmall']
 var audioFiles = ['taisho-echo', 'squeak', 'hum-04', 'bells', 'fridge2', 'gogogo', 'crackle', 'epic2', 'chromatic2', 'epic1', 'chromatic1', 'organ']
-var imageFiles = ['white', 'fridge-template','fridge-template-2', 'fridge-template-3', 'blank', 'fridge', 'beer', 'face']
+var imageFiles = ['bread', 'white', 'radio', 'tokyo', 'byuo', 'mosquito', 'hana', 'fridge-template','fridge-template-2', 'fridge-template-3', 'blank', 'fridge', 'beer', 'face']
 
 var scenes = [
   /////ACT 1 (15 scenes)/////////////
@@ -20,12 +20,12 @@ var scenes = [
   {video: 'drillsmall-video', audio: 'chromatic1-audio'},
   {video: 'drillsmall-video', audio: 'chromatic2-audio'},
   /////ACT 2 (9 scenes)/////////////
-  {video: 'beer-image', audio: 'gogogo-audio'},
-  {video: 'beer-image', audio: 'gogogo-audio'},
-  {video: 'beer-image', audio: 'gogogo-audio'},
-  {video: 'face-image', audio: 'gogogo-audio'},
-  {video: 'face-image', audio: 'gogogo-audio'},
-  {video: 'face-image', audio: 'gogogo-audio'},
+  {video: 'hana-image', audio: 'gogogo-audio'},
+  {video: 'hana-image', audio: 'gogogo-audio'},
+  {video: 'hana-image', audio: 'gogogo-audio'},
+  {video: 'bread-image', audio: 'gogogo-audio'},
+  {video: 'bread-image', audio: 'gogogo-audio'},
+  {video: 'bread-image', audio: 'gogogo-audio'},
   {video: 'beer-image', audio: 'gogogo-audio'},
   {video: 'beer-image', audio: 'gogogo-audio'},
   {video: 'beer-image', audio: 'gogogo-audio'},
@@ -36,17 +36,17 @@ var scenes = [
   {video: 'fridge-template-3-image', audio: ''},
   {video: 'fridge-template-2-image', audio: 'hum-04-audio'},
   /////ACT 4 (12 scenes)/////////////
-  {video: 'blank-image', audio: 'taisho-echo-audio'},
-  {video: 'blank-image', audio: 'taisho-echo-audio'},
-  {video: 'blank-image', audio: 'taisho-echo-audio'},
-  {video: 'blank-image', audio: 'taisho-echo-audio'},
-  {video: 'blank-image', audio: 'taisho-echo-audio'},
-  {video: 'blank-image', audio: 'taisho-echo-audio'},
-  {video: 'blank-image', audio: 'taisho-echo-audio'},
-  {video: 'blank-image', audio: 'taisho-echo-audio'},
-  {video: 'blank-image', audio: 'taisho-echo-audio'},
-  {video: 'blank-image', audio: 'taisho-echo-audio'},
-  {video: 'blank-image', audio: 'taisho-echo-audio'},
-  {video: 'blank-image', audio: 'taisho-echo-audio'}
+  {video: 'tokyo-image', audio: 'taisho-echo-audio'},
+  {video: 'radio-image', audio: 'taisho-echo-audio'},
+  {video: 'byuo-image', audio: 'taisho-echo-audio'},
+  {video: 'mosquito-image', audio: 'taisho-echo-audio'},
+  {video: 'tokyo-image', audio: 'taisho-echo-audio'},
+  {video: 'radio-image', audio: 'taisho-echo-audio'},
+  {video: 'byuo-image', audio: 'taisho-echo-audio'},
+  {video: 'mosquito-image', audio: 'taisho-echo-audio'},
+  {video: 'tokyo-image', audio: 'taisho-echo-audio'},
+  {video: 'radio-image', audio: 'taisho-echo-audio'},
+  {video: 'byuo-image', audio: 'taisho-echo-audio'},
+  {video: 'mosquito-image', audio: 'taisho-echo-audio'}
 
   ]
