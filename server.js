48407ca21b2bd708964cd93f80c0037d4c761bf2
Sam Andreae <sandreae@gmail.com>
2018-01-12 16:43:22 +0100

    shorter time

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
 
 const io = socketIO(server);
 
-var myVar = setInterval(myTimer, 20000);
+var myVar = setInterval(myTimer, 10);
 
 function myTimer() {
       io.emit("stop", track);
